5c2865b08112ae279e90b79e2fd7f7d10ba78426
EunOh <bdohhhhh@gmail.com>
2025-04-22 21:58:40 +0900

    [add]: api/history

---
+++ app/api/history/route.ts
@@ -0,0 +1,19 @@
+import { getChatsByUserId } from "@/lib/db/queries";
+import { createClient } from "@/lib/supabase/server";
+
+export async function GET() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (error || !user) {
+    return Response.json("Unauthorized!", { status: 401 });
+  }
+
+  // biome-ignore lint: Forbidden non-null assertion.
+  const chats = await getChatsByUserId({ id: user.id! });
+  return Response.json(chats);
+}
