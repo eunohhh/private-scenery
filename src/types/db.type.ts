1a3a14dcec5ce5757106da321700a490bfd701e4
EunOh <bdohhhhh@gmail.com>
2025-04-22 22:01:47 +0900

    [add]: types

---
+++ src/types/db.type.ts
@@ -0,0 +1,20 @@
+import type { Json } from "./supabase";
+import { Tables } from "./supabase";
+
+export type Message = Tables<"message">;
+export type Chat = Tables<"chat">;
+
+export type ChatHistoryResponse = {
+  data: Chat[];
+  error: string | null;
+  status: number;
+  statusText: string;
+};
+
+export type DBMessage = {
+  id: string;
+  role: string;
+  content: Json;
+  created_at: string;
+  chatId: string;
+};
