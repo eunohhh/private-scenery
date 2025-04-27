203b3ec9af12638faaeeaee1a35f2a9b251939a5
EunOh <bdohhhhh@gmail.com>
2025-04-27 21:06:34 +0900

    [update]: db type

--- src/types/db.type.ts
+++ src/types/db.type.ts
@@ -3,18 +3,19 @@ import { Tables } from "./supabase";
 
 export type Message = Tables<"message">;
 export type Chat = Tables<"chat">;
+export type Scenery = Tables<"scenery">;
 
 export type ChatHistoryResponse = {
-	data: Chat[];
-	error: string | null;
-	status: number;
-	statusText: string;
+  data: Chat[];
+  error: string | null;
+  status: number;
+  statusText: string;
 };
 
 export type DBMessage = {
-	id: string;
-	role: string;
-	content: Json;
-	created_at: string;
-	chatId: string;
+  id: string;
+  role: string;
+  content: Json;
+  created_at: string;
+  chatId: string;
 };
